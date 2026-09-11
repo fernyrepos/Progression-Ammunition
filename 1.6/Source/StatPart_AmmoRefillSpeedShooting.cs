0d5eafdca852302135a6eecc064929d5ab6a5c07
IC Linnun DEU <github@danielfigge.net>
2026-09-11 19:24:51 +0200

    ammo refill speed scales with shooting skill

---
+++ 1.6/Source/StatPart_AmmoRefillSpeedShooting.cs
@@ -0,0 +1,36 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ProgressionAmmunition
+{
+    public class StatPart_AmmoRefillSpeedShooting : StatPart
+    {
+        private const float AmmoRefillSpeedBonusPerShootingSkillLevel = 0.025f;
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (!req.HasThing)
+                return;
+
+            Pawn pawn = req.Thing as Pawn;
+            if (pawn?.skills?.GetSkill(SkillDefOf.Shooting) == null)
+                return;
+
+            float bonus = pawn.skills.GetSkill(SkillDefOf.Shooting).Level * AmmoRefillSpeedBonusPerShootingSkillLevel;
+            val += bonus;
+        }
+
+        public override string ExplanationPart(StatRequest req)
+        {
+            if (!req.HasThing)
+                return null;
+
+            Pawn pawn = req.Thing as Pawn;
+            if (pawn?.skills?.GetSkill(SkillDefOf.Shooting) == null)
+                return null;
+
+            float bonus = pawn.skills.GetSkill(SkillDefOf.Shooting).Level * AmmoRefillSpeedBonusPerShootingSkillLevel;
+            return $"Shooting skill: +{bonus * 100f:0.#}%";
+        }
+    }
+}
